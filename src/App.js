bb5706bcb2d6b99354a21db0e9c4d39deee66737
princehabeeb <habiy3400@gmail.com>
2023-02-11 00:54:06 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
+
+import { Routes, Route } from 'react-router-dom'
 const App = () => {
-  return <div>react apps</div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
 };
 
 export default App;  
