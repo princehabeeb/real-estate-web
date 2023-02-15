12bb3809c1170b737591300fdef607ab8f1c4714
princehabeeb <habiy3400@gmail.com>
2023-02-16 00:54:38 +0100

    new commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,16 @@ import Logo from '../assets/img/logo.svg';
 
 const Header = () => {
   return (
-  <header>
+  <header className='py-6 mb-12 border-b'>
     <div className='container mx-auto'>
       {/* Logo */}
       <Link to='/'>
          <img src={Logo} alt='' />
       </Link>
+      <div>
+        <Link to=''>Log in</Link>
+        <Link to=''>Sign up</Link>
+      </div>
     </div>
   </header>
   );
