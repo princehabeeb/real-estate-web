efe4415a162f2eb7fd7158940406b64e29a0292f
princehabeeb <habiy3400@gmail.com>
2023-02-16 15:50:15 +0100

    new

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import Image from '../assets/img/house-banner.png';
+import Search from '../components/Search';
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+  <section>
+       <div>
+           <div>
+            <h1><span>Rent</span> Your Dream House With Us.</h1>
+             <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Necessitatibus sapiente error, blanditiis incidunt aperiam ab 
+              nostrum voluptatibus earum dignissimos sed!
+              </p>
+           </div>
+           {/* images */}
+           <div>
+            <img src={Image} alt="" />
+           </div>
+       </div>
+       <Search />
+  </section>
+  );
 };
 
 export default Banner;
