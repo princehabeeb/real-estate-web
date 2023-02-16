bb773c6830484c67ebc8dc52f0e5de4b1650baeb
princehabeeb <habiy3400@gmail.com>
2023-02-16 01:03:20 +0100

    new changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,17 @@ import Logo from '../assets/img/logo.svg';
 const Header = () => {
   return (
   <header className='py-6 mb-12 border-b'>
-    <div className='container mx-auto'>
+    <div className='container mx-auto flex justify-between items-center'>
       {/* Logo */}
       <Link to='/'>
          <img src={Logo} alt='' />
       </Link>
-      <div>
-        <Link to=''>Log in</Link>
-        <Link to=''>Sign up</Link>
+      <div className='flex items-center gap-6'>
+        <Link to='' className='hover:text-violet-500 transition'>Log in</Link>
+        <Link to=''className=
+        'bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition' >
+          Sign up
+          </Link>
       </div>
     </div>
   </header>
