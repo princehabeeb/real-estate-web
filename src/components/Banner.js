724683750a9861012f6327cfc1ab3fb2cdb73215
princehabeeb <habiy3400@gmail.com>
2023-02-17 23:54:54 +0100

    new

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
   return (
   <section className='h-full max-h-[640px] mb-8 xl:mb-24'>
        <div className='flex flex-col lg:flex-row'>
-           <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center'>
+           <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start'>
             <h1><span>Rent</span> Your Dream House With Us.</h1>
              <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
