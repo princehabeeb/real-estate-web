641507d8ec7c2a35355c1825f1be5accca8ace32
princehabeeb <habiy3400@gmail.com>
2023-02-18 00:41:37 +0100

    next

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import CountryDropdown from '../components/CountryDropdown';
 import PropertyDropdown from '../components/PropertyDropdown';
+import PriceRangeDropdown from '../components/PriceRangeDropdown';
 
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+  <div>
+    <CountryDropdown />
+    <PropertyDropdown />
+    <PriceRangeDropdown />
+  </div>
+  );
 };
 
 export default Search;
