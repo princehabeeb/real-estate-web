d47217c680f65e399e6ef389ed06588b86a2b6a0
princehabeeb <habiy3400@gmail.com>
2023-02-18 00:30:34 +0100

    next

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,17 +9,18 @@ const Banner = () => {
            <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center 
            lg:items-start text-center lg:text-left justify-center flex-1 px-4
            lg:px-0'>
-            <h1 className='text-4xl lg:text-[58px] font-semibold leading-none'>
-              <span>Rent</span> Your Dream House With Us.
+            <h1 className='text-4xl lg:text-[58px] font-semibold
+             leading-none mb-6'>
+              <span className='text-violet-700'>Rent</span> Your Dream House With Us.
             </h1>
-             <p>
+             <p className='max-w-[480px] mb-8'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Necessitatibus sapiente error, blanditiis incidunt aperiam ab 
               nostrum voluptatibus earum dignissimos sed!
               </p>
            </div>
            {/* images */}
-           <div>
+           <div className='hidden flex-1 lg:flex'>
             <img src={Image} alt="" />
            </div>
        </div>
