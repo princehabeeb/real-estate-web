40dbd6f33894566f174acc2eaa1a8cf8ee2d32a2
princehabeeb <habiy3400@gmail.com>
2023-02-17 23:24:49 +0100

    new commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,9 +4,9 @@ import Search from '../components/Search';
 
 const Banner = () => {
   return (
-  <section>
-       <div>
-           <div>
+  <section className='h-full max-h-[640px] mb-8 xl:mb-24'>
+       <div className='flex flex-col lg:flex-row'>
+           <div className='lg:ml-8 xl:ml-[135px] flex flex-col items-center'>
             <h1><span>Rent</span> Your Dream House With Us.</h1>
              <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
