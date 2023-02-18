79c1b62e1fbb04863bffaef27122c679ea58f126
princehabeeb <habiy3400@gmail.com>
2023-02-18 22:32:58 +0100

    search.js edit

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,7 +9,7 @@ const Search = () => {
   return (
   <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col
   lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4
-  lg:shadow-1 bg-white lg:bg-transparent lg:backdrop-blur'>
+  lg:shadow-1 bg-white lg:bg-transparent lg:backdrop-blur rounded-lg'>
     <CountryDropdown />
     <PropertyDropdown />
     <PriceRangeDropdown />
