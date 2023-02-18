59217624ef70792d5b6f9168a53c7bd58030369f
princehabeeb <habiy3400@gmail.com>
2023-02-18 22:30:21 +0100

    edited search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,14 +2,18 @@ import React from 'react';
 import CountryDropdown from '../components/CountryDropdown';
 import PropertyDropdown from '../components/PropertyDropdown';
 import PriceRangeDropdown from '../components/PriceRangeDropdown';
+import {RiSearch2Line} from 'react-icons/ri'
 
 
 const Search = () => {
   return (
-  <div>
+  <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col
+  lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4
+  lg:shadow-1 bg-white lg:bg-transparent lg:backdrop-blur'>
     <CountryDropdown />
     <PropertyDropdown />
     <PriceRangeDropdown />
+    <button><RiSearch2Line /></button>
   </div>
   );
 };
