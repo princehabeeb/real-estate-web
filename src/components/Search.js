da7fa316391b4c3c131f57b4c00453f14407c338
princehabeeb <habiy3400@gmail.com>
2023-02-19 13:05:37 +0100

    new

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,7 +13,11 @@ const Search = () => {
     <CountryDropdown />
     <PropertyDropdown />
     <PriceRangeDropdown />
-    <button><RiSearch2Line /></button>
+    <button className='bg-violet-700 hover:bg-violet-800
+    transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center
+    text-white text-lg'>
+      <RiSearch2Line />
+    </button>
   </div>
   );
 };
