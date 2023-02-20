4716b42b1dec64cc2e73043bf5b53400a70a41db
princehabeeb <habiy3400@gmail.com>
2023-02-20 17:04:07 +0100

    new

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,9 +1,11 @@
-import React, {useState, useEffect, createContext} from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import {housesData} from '../data';
 
+export const HouseContext = createContext();
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+const HouseContextProvider = ({ children }) => {
+  return  <HouseContext.Provider value={console.log('this is the console')} > {children} </HouseContext.Provider>
+;
 };
 
-export default HouseContext;
+export default HouseContextProvider;
