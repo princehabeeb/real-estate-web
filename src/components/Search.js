804823ad8a4a209a1d4fe204583d18671f3827bb
princehabeeb <habiy3400@gmail.com>
2023-02-20 17:45:47 +0100

    new

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,14 @@ import CountryDropdown from '../components/CountryDropdown';
 import PropertyDropdown from '../components/PropertyDropdown';
 import PriceRangeDropdown from '../components/PriceRangeDropdown';
 import {RiSearch2Line} from 'react-icons/ri'
+import { useContext } from 'react';
+import { HouseContext } from './HouseContext';
 
 
 const Search = () => {
+  const {houses} = useContext(HouseContext);
+  
+
   return (
   <div className='px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col
   lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4
