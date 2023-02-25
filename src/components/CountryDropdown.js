9bc5982cfa783a73c36378e3eef9502575074ef3
princehabeeb <habiy3400@gmail.com>
2023-02-25 15:10:33 +0100

    new

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -11,12 +11,19 @@ const CountryDropdown = () => {
    
    const [isOpen, setIsOpen] = useState(false);
 
-  return <Menu as='div' className='dropdown relative' >
-    <Menu.Button>
+  return <Menu as='div' onClick={() => setIsOpen(!isOpen)} className='dropdown relative' >
+    <Menu.Button className='dropdown-btn w-full text-left'>
       <RiMapPinLine className='dropdown-icon-primary' />
       <div>
         <div>{country}</div>
         <div>Select your place</div>
+        {
+          isOpen ? (
+            <RiArrowUpSLine className='dropdown-icon-primary' />
+          ): (
+            <RiArrowDownSLine className='dropdown-icon-primary' />
+          )
+        }
       </div>
     </Menu.Button>
   </Menu>;
