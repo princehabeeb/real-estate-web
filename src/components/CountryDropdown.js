5d1de333dd33b2d9fd40eee35132ed80afe3be6b
princehabeeb <habiy3400@gmail.com>
2023-02-25 15:29:05 +0100

    new commit

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -8,24 +8,38 @@ import { HouseContext } from './HouseContext';
 const CountryDropdown = () => {
   const {country, setCountry, countries } =
    useContext(HouseContext);
-   
+   console.log(countries);  
    const [isOpen, setIsOpen] = useState(false);
 
   return <Menu as='div' onClick={() => setIsOpen(!isOpen)} className='dropdown relative' >
     <Menu.Button className='dropdown-btn w-full text-left'>
       <RiMapPinLine className='dropdown-icon-primary' />
       <div>
-        <div>{country}</div>
-        <div>Select your place</div>
-        {
+        <div className='text-[15px] font-medium leading-tight'>
+          {country}
+          </div>
+        <div className='text-[13px]'>Select your place</div>
+      </div>
+      {
           isOpen ? (
-            <RiArrowUpSLine className='dropdown-icon-primary' />
+            <RiArrowUpSLine className='dropdown-icon-secondary' />
           ): (
-            <RiArrowDownSLine className='dropdown-icon-primary' />
+            <RiArrowDownSLine className='dropdown-icon-secondary' />
           )
         }
-      </div>
     </Menu.Button>
+    <Menu.Items>
+      {countries.map((country, index) => {
+        return (
+          <Menu.Item 
+          onClick={() => setCountry(country)}
+          className='coursor-pointer hover:text-violet-700 transition'
+           as='li' key={index}>
+              {country}
+          </Menu.Item>
+        );
+      })}
+    </Menu.Items>
   </Menu>;
 };
 
