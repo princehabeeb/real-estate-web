42eb557a0c97f99d3aeeb54d7c6b6a9e349d3133
princehabeeb <habiy3400@gmail.com>
2023-03-09 06:55:32 +0100

    nest

--- src/components/House.js
+++ src/components/House.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { BiBed, BiBath, BiArea } from 'react-icons/bi';
 
 const House = ({house}) => {
-  return <div>House</div>;
+  const { image, type, country, address, bedrooms, bathrooms,
+  surface, price } = house;
+  return (
+  <div>
+   <img src={image} alt="" />
+   <div>
+    <div> {type}</div>
+    <div> {country}</div>
+   </div>
+    </div>
+    );
 };
 
 export default House;
