853f90197e09d7c81710a01e84aa04f122eef76d
princehabeeb <habiy3400@gmail.com>
2023-03-09 07:12:48 +0100

    now

--- src/components/House.js
+++ src/components/House.js
@@ -5,11 +5,15 @@ const House = ({house}) => {
   const { image, type, country, address, bedrooms, bathrooms,
   surface, price } = house;
   return (
-  <div>
-   <img src={image} alt="" />
-   <div>
-    <div> {type}</div>
-    <div> {country}</div>
+  <div className='bg-white shadow-1 p-5 rounded-lg rounded-t1-[90px]
+   w-full max-w-[352px] mx-auto coursor-pointer hover:shadow-2xl
+   transition'>
+   <img className='mb-8' src={image} alt="" />
+   <div className='mb-4 flex gap-x-2 text-sm'>
+    <div className='bg-green-500 rounded-full text-white px-3'> 
+    {type}</div>
+    <div className='bg-violet-500 rounded-full text-white px-3'> 
+      {country}</div>
    </div>
     </div>
     );
