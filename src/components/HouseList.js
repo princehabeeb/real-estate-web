feca15e6d6d82a7b5d4bd3f4c77407dbf184bad6
princehabeeb <habiy3400@gmail.com>
2023-03-16 22:40:30 +0100

    new commit

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -9,8 +9,15 @@ const HouseList = () => {
   const {houses, loading} = useContext(HouseContext);
   if (loading){
     return (<ImSpinner2 className='mx-auto animate-spin text-violet-700
-    text-4xl mt-[200px]'/>)
+    text-4xl mt-[200px]'/>
+    )
   }
+
+  if (houses.length < 1) {
+    return <div>Sorry, nothing found</div>
+  }
+
+
   return (<section className='mb-20'>
     <div className="container mx-auto">
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-14'>
